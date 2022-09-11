a11d3e87c82161d6c5dcfb0b3352a47d1926b8ff
Geordy <ggeordymm@gmail.com>
2022-09-10 19:23:25 -0500

    First commit

---
+++ src/com/init/mainTest.java
@@ -0,0 +1,68 @@
+package com.init;
+
+public class mainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int numeroif = -4;
+		
+		if(numeroif>0) {
+			System.out.println("Es positivo");
+		} else if(numeroif==0) {
+			System.out.println("Es cero");
+		} else if(numeroif<0) {
+			System.out.println("Es negativo");
+		}
+		int numerowhile = 0;
+		
+		
+		while(numerowhile < 3) {
+			numerowhile++;
+			System.out.println(numeroif);
+		}
+		
+		do{
+			System.out.println("Ejecucion 1");
+		} while(numerowhile < 3);
+		
+		
+		
+		for(int numerofor = 0; numerofor <= 3; numerofor++) {
+			System.out.println("numerofor");
+		}	
+		
+		String estacion = "";
+		
+		switch(estacion) {
+		case "otoño": 
+			
+			System.out.println("Es otoño");
+		break;
+		
+		case "invierno":
+			System.out.println("Es invierno");
+			break;
+			
+		case "verano":
+			System.out.println("Es verano");
+			break;
+			
+		case "primavera":
+			System.out.println("Es primavera");
+			break;
+			
+			
+			default:
+				System.out.println("No es una estacion");
+				break;
+		}
+
+}
+
+	
+	
+}
+
+
